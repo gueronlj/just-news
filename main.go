d9ee9b466622eb8d0718546e29b13b41d89c4445
Lorens Gueron <gueronlj@gmail.com>
2024-11-14 19:54:34 -0500

    Added: single article scoring

--- main.go
+++ main.go
@@ -1,16 +1,140 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 
-	"github.com/labstack/echo/v4"
+	"github.com/gocolly/colly"
 )
 
+var adjectiveMap = make(map[string]int)
+
 func main() {
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
-	app := echo.New()
+	// app := echo.New()
+	// app.Logger.Fatal(
+	// 	app.Start(os.Getenv("PORT")),
+	// )
 
-	app.Logger.Fatal(
-		app.Start(os.Getenv("PORT")),
-	)
+	//var adjectiveMap map[string]int
+	//adjectiveMap = make(map[string]int)
+
+	loadData()
+	scrape()
+}
+
+func loadData() {
+	//open local text file
+	file, err := os.Open("wordmap.txt")
+	if err != nil {
+		fmt.Println("Error opening wordmap.txt", err)
+		return
+	}
+	defer file.Close()
+
+	//scan file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		//skip blank lines
+		if line == "" {
+			continue
+		}
+		//split the line 'word:score'
+		split := strings.Split(line, ":")
+		//trim white space
+		trimmed := strings.TrimSpace(split[1])
+		//convert score to integer
+		num, err := strconv.Atoi(trimmed)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//map each word:score
+		word := split[0]
+		score := num
+		adjectiveMap[word] = score
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+}
+
+type Article struct {
+	Words      int
+	Paragraphs int
+	Score      int
+	URL        string
+}
+
+func scrape() {
+	wordCount := 0
+	paragraphCount := 0
+	wordsChecked := 0
+	score := 0
+	quoteCount := 0
+
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Something went wrong:", err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited", r.Request.URL)
+	})
+	//CNN: ".vossi-paragraph"
+	c.OnHTML(".article-body p", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+		split := strings.Split(e.Text, " ")
+
+		possibleQuote := strings.Contains(split[0], `"`)
+		if possibleQuote {
+			endingQuote := strings.Contains(split[len(split)-1], `"`)
+			isQuote := strings.Contains(split[len(split)-1], "said.")
+
+			if endingQuote || isQuote {
+				fmt.Println("QUOTE ALERT!!!")
+				quoteCount++
+			}
+			//skip this paragraph
+		}
+		//we need _ here because golang is expecting us to care about the index, we dont here so we use _
+		for _, currentWord := range split {
+			value, exists := adjectiveMap[currentWord]
+			if exists {
+				wordsChecked++
+				score += value
+			}
+			wordCount++
+		}
+		paragraphCount++
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+	//CNN
+	//c.Visit("https://www.cnn.com/2024/11/14/politics/todd-blanche-deputy-attorney-general/index.html")
+	c.Visit("https://www.foxnews.com/us/special-education-teacher-resigns-apologizes-viral-video-threatening-trump-voters-sparks-backlash")
+
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	// 	e.Request.Visit(e.Attr("href"))
+	// })
+
+	fmt.Println(wordCount, paragraphCount, wordsChecked, score)
+	fmt.Println("quotes: ", quoteCount)
 }
